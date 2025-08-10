24a06b6d3291c1d3cb7e82a49f69d760bdfe77e1
magnolia.gcloud <magnolia.gcloud@gmail.com>
2025-08-10 16:58:58 +0000

    chore(sabaody): add project

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -32,4 +35,4 @@ function App() {
   )
 }
 
-export default App
+export default App  
